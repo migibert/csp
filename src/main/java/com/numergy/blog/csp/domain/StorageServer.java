9c46b3a7d8ae46defa21cfa819e651f60f9c273e
mikael.gibert <mikael.gibert@gmail.com>
2014-05-17 21:37:54 +0200

    * Ajout du modèle objet * Ajout de TUs * Ajout de factories utilitaires

---
+++ src/main/java/com/numergy/blog/csp/domain/StorageServer.java
@@ -0,0 +1,13 @@
+package com.numergy.blog.csp.domain;
+
+public class StorageServer {
+	private int capacity;
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+}
---
+++ src/main/java/com/numergy/blog/csp/domain/VirtualVolume.java
@@ -0,0 +1,23 @@
+package com.numergy.blog.csp.domain;
+
+public class VirtualVolume {
+	private int capacity;
+	private int usedCapacity;
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getUsedCapacity() {
+		return usedCapacity;
+	}
+
+	public void setUsedCapacity(int usedCapacity) {
+		this.usedCapacity = usedCapacity;
+	}
+
+}
